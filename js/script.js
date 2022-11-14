8ad867ee396871ce20f4fbd0a3d0f7f90648967e
peding123 <nastyraven123@gmail.com>
2022-11-15 05:59:07 +0700

    second

--- js/script.js
+++ js/script.js
@@ -37,3 +37,22 @@ window.addEventListener("load", vanish);
 function vanish() {
   loader.classList.add("disppear");
 }
+
+// lazy loading
+
+const images = document.querySelectorAll("img");
+
+const imgOptions = {};
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+
+    const img = entry.target;
+    img.src = img.src.replace("w=10&", "w=800&");
+    imgObserver.unobserve(entry.target);
+  });
+}, imgOptions);
+
+images.forEach((img) => {
+  imgObserver.observe(img);
+});
