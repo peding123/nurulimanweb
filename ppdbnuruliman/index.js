1446f91c2a0e1b8cf7d593db8715229113550bd9
peding123 <nastyraven123@gmail.com>
2022-11-14 18:18:11 +0700

    first commit

---
+++ ppdbnuruliman/index.js
@@ -0,0 +1,26 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbycv9P9IylbhU4tdv3FVIXUEJAr6aDUcFza_y8r1NWY35db2wiZOfQH7G6-tpXMmsC0EQ/exec";
+const form = document.forms["form-pendaftaran"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // ketika tombol submit di klik
+  // tampilkan tombol loading, hilangkan tombol kirim
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      // tampilkan tombol kirim, hilangkan tombol loading
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      // tampilkan alert
+      myAlert.classList.toggle("d-none");
+      // reset form
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
